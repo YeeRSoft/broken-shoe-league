ca7bafb18dc1a2001c82f4b5939fd849ba0bfb49
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-10-13 23:57:47 -0400

    configure Web API to return jsons

--- Source/BrokenShoeLeague.Web.API/Global.asax.cs
+++ Source/BrokenShoeLeague.Web.API/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BrokenShoeLeague.Data;
 
 namespace BrokenShoeLeague.Web.API
 {
@@ -13,8 +14,12 @@ namespace BrokenShoeLeague.Web.API
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);

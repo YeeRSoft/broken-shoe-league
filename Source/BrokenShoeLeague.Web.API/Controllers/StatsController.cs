91a5cfca4181932c39973d43199d004cc4f13a98
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-10-30 10:23:02 -0700

    create stats api controller and player ranking stats view model

---
+++ Source/BrokenShoeLeague.Web.API/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BrokenShoeLeague.Domain.Repositories;
+
+namespace BrokenShoeLeague.Web.API.Controllers
+{
+    public class StatsController : ApiController
+    {
+        private readonly IBrokenShoeLeagueRepository _brokenShoeLeagueRepository;
+
+        public StatsController(IBrokenShoeLeagueRepository brokenShoeLeagueRepository)
+        {
+            _brokenShoeLeagueRepository = brokenShoeLeagueRepository;
+        }
+
+        public IHttpActionResult GetRanking(int seasonId)
+        {
+            var season = _brokenShoeLeagueRepository.GetSeasonById(seasonId);
+
+            if (season == null)
+                return NotFound();
+
+            var seasonPlayers= _brokenShoeLeagueRepository.GetAllPlayers()
+                .Where(p => p.PlayerRecords.Any(pr => pr.MatchDay.Season.Id == seasonId));
+
+            return Ok();
+        }
+    }
+}
---
+++ Source/BrokenShoeLeague.Web.API/Models/PlayerRankingStatsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrokenShoeLeague.Web.API.Models
+{
+    public class PlayerRankingStatsViewModel
+    {
+        public string PlayerName { get; set; }
+        public string ImageUrl { get; set; }
+        public double Performance { get; set; }
+        public int Won { get; set; }
+        public int Lost { get; set; }
+        public int Tied { get; set; }
+        public int Goals { get; set; }
+        public int Assists { get; set; }
+    }
+}

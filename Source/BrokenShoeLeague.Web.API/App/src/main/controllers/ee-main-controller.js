8aacb0228ed2ac007056994bb6c65c7c866e6809
ercpereda <ercp42@gmail.com>
2015-10-30 11:12:53 -0400

    implemented main controller

--- Source/BrokenShoeLeague.Web.API/App/src/main/controllers/ee-main-controller.js
+++ Source/BrokenShoeLeague.Web.API/App/src/main/controllers/ee-main-controller.js
@@ -3,9 +3,36 @@
         .module('eeBrokenShoeLeague')
         .controller('eeMainController', eeMainController);
 
-
+    eeMainController.$inject = ['$mdSidenav', '$location'];
 
     function eeMainController() {
-        
+        var vm = this;
+
+        vm.go = function (url) { $location.path(url); }
+        vm.toggleSidenav = function (sideNavId) { $mdSidenav(sideNavId).toggle(); }        
+        vm.sideNavItems = [
+            {
+                name: "General",
+                items: [
+                    { name: "Dashboard", url: "", icon: "" },
+                    { name: "Gallery", url: "", icon: "" },
+                    { name: "News", url: "", icon: ""}
+                ]
+            },
+            {
+                name: "User",
+                items: [
+                  { name: "Profile", url: "", icon: "" },
+                  { name: "Messages", url: "", icon: ""}
+                ]
+            },
+            {
+                name: "Statistics",
+                items: [
+                    { name: "Match Day Results", url: "", icon: ""},
+                    { name: "Ranking", url: "", icon: "" }
+                ]
+            }
+        ];
     }
 })()

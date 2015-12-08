68542c3cc42686a1e1c7b747b9366af787bedd5b
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-12-07 22:32:43 -0500

    update menu items

--- Source/BrokenShoeLeague.Web.API/BSL.Client/app/home/HomeController.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/app/home/HomeController.js
@@ -12,21 +12,31 @@
             $mdSidenav(menuId).toggle();
         };
         vm.menu = [
-        {
-            link: '',
-            title: 'Dashboard',
-            icon: 'dashboard'
-        },
-        {
-            link: '',
-            title: 'Friends',
-            icon: 'group'
-        },
-        {
-            link: '',
-            title: 'Messages',
-            icon: 'message'
-        }
+            {
+                link: '',
+                title: 'Dashboard',
+                icon: 'dashboard'
+            },
+            {
+                link: '',
+                title: 'Gallery',
+                icon: 'view_carousel'
+            },
+            {
+                link: '',
+                title: 'News',
+                icon: 'message'
+            },
+            {
+                link: '',
+                title: 'Matchday Results',
+                icon: 'today'
+            },
+            {
+                link: '',
+                title: 'Ranking',
+                icon: 'view_list'
+            }
         ];
         vm.admin = [
           {
@@ -39,38 +49,6 @@
               title: 'Settings',
               icon: 'settings'
           }
-        ];
-        vm.activity = [
-            {
-                what: 'Brunch this weekend?',
-                who: 'Ali Conners',
-                when: '3:08PM',
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                what: 'Summer BBQ',
-                who: 'to Alex, Scott, Jennifer',
-                when: '3:08PM',
-                notes: "Wish I could come out but I'm out of town this weekend"
-            },
-            {
-                what: 'Oui Oui',
-                who: 'Sandra Adams',
-                when: '3:08PM',
-                notes: "Do you have Paris recommendations? Have you ever been?"
-            },
-            {
-                what: 'Birthday Gift',
-                who: 'Trevor Hansen',
-                when: '3:08PM',
-                notes: "Have any ideas of what we should get Heidi for her birthday?"
-            },
-            {
-                what: 'Recipe to try',
-                who: 'Brian Holt',
-                when: '3:08PM',
-                notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
-            },
         ];
     }
 })()

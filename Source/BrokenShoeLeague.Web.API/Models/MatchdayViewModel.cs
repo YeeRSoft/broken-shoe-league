b51182bd3696b84fb41b8b2c74aa423b72122f7c
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2015-12-14 13:52:21 -0500

    create season and matchday view model classes

---
+++ Source/BrokenShoeLeague.Web.API/Models/MatchdayViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrokenShoeLeague.Domain;
+
+namespace BrokenShoeLeague.Web.API.Models
+{
+    public class MatchdayViewModel
+    {
+        public MatchdayViewModel(Matchday matchday)
+        {
+            Date =matchday.Date;
+            Number = matchday.Number;
+            PlayerStats = matchday.PlayerStats.Select(x => new PlayerRankingStatsViewModel(x));
+        }
+
+        public IEnumerable<PlayerRankingStatsViewModel> PlayerStats { get; set; }
+
+        public DateTime Date { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Source/BrokenShoeLeague.Web.API/Models/SeasonViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrokenShoeLeague.Domain;
+
+namespace BrokenShoeLeague.Web.API.Models
+{
+    public class SeasonViewModel
+    {
+        public SeasonViewModel(Season season)
+        {
+            Name = season.Name;
+            Matchdays = season.MatchDays.Select(x => new MatchdayViewModel(x));
+        }
+
+        public IEnumerable<MatchdayViewModel> Matchdays { get; set; }
+
+        public string Name { get; set; }
+    }
+}

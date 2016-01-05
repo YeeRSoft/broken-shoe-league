8c04e11f50f718b21a62ab49881f99e6f22eb61c
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-04 22:20:42 -0500

    remove average function from player model

--- Source/BrokenShoeLeague.Domain/Player.cs
+++ Source/BrokenShoeLeague.Domain/Player.cs
@@ -36,11 +36,6 @@ namespace BrokenShoeLeague.Domain
                 return PlayerRecords.Count(x => Math.Abs(x.Performance - x.Matchday.PlayerStats.Max(ps => ps.Performance)) < 0.0001);
             }
         }
-        /*We shouldn't calculate this. It's not useful.*/
-        public double AveragePerformance
-        {
-            get { return PlayerRecords.Any() ? PlayerRecords.Average(x => x.Performance) : 0; }
-        }
 
         public void Update(string name, string imageProfileUrl, bool enabled)
         {

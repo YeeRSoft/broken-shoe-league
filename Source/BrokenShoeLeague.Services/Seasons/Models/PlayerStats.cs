185353951d2cc7feacafbf92c805b1ac9a25ab32
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-04 22:46:16 -0500

    players stats shouldn't have the definition of the calculate performance function. use the function defined in utils instead. there is no need to set the performance when the stats are being loaded in the importer service.

--- Source/BrokenShoeLeague.Services/Seasons/Models/PlayerStats.cs
+++ Source/BrokenShoeLeague.Services/Seasons/Models/PlayerStats.cs
@@ -5,7 +5,12 @@ namespace BrokenShoeLeague.Services.Seasons.Models
     {
         public int PlayerId { get; set; }
         public string PlayerName { get; set; }
-        public int PlayedGames { get; set; }
+
+        public int PlayedGames
+        {
+            get { return Wins + Draws + Losts; }
+        }
+
         public int Wins { get; set; }
         public int Draws { get; set; }
         public int Losts { get; set; }
@@ -22,17 +27,6 @@ namespace BrokenShoeLeague.Services.Seasons.Models
             if (2 * PlayedGames < Goals)
                 return false;
             return true;
-        }
-
-        //Calculates the performance
-        public void CalculatePerformance()
-        {
-            const double wGoals = 0.5;
-            const double wAssists = 0.25;
-            const double wWin = 0.2;
-            const double wDraw = 1 / 15.0;
-            const double wLost = 0.1;
-            Performance = Math.Round((wWin * Wins + wDraw * Draws - wLost * Losts + wGoals * Goals + wAssists * Assists) / PlayedGames, 3);
         }
     }
 }

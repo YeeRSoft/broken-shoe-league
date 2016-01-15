4ff8dd503fbc2dadd5273f30f1ec13427b30fcce
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-15 09:24:23 -0500

    define interface that will be used to implement all the achievements

---
+++ Source/BrokenShoeLeague.Domain/Interfaces/Common/IAchievement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrokenShoeLeague.Domain.Enums;
+
+namespace BrokenShoeLeague.Domain.Interfaces.Common
+{
+    public interface IAchievement
+    {
+        string GetName();
+        string GetIconName();
+        string Text(string username);
+        bool MatchCondition(IEnumerable<PlayerRecord> stats);
+    }
+}

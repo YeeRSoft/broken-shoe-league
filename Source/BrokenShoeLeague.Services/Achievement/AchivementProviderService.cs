d3c52a94bc38d5c1b043e1d5ec1ec3629506d9d9
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-15 09:25:20 -0500

    create service to retrieve available achievements

---
+++ Source/BrokenShoeLeague.Services/Achievement/AchivementProviderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrokenShoeLeague.Services.Achievement.Models;
+
+namespace BrokenShoeLeague.Services.Achievement
+{
+    public class AchivementProviderService : IAchivementProviderService
+    {
+        public IEnumerable<AchievementWrapper> GetAchievements()
+        {
+            return Enumerable.Empty<AchievementWrapper>();
+        }
+    }
+}

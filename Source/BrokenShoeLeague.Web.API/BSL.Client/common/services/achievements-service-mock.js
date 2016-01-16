846d39ec706b400b2990f45d7af72bf46fc453be
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-16 10:07:18 -0500

    create angular service and mock for retrieve available achievements from the server

--- Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service-mock.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service-mock.js
@@ -1,32 +1,31 @@
-﻿(function () {
+﻿(function() {
     "use strict";
 
-    (function() {
-        "use strict";
+    var app = angular
+        .module("achievementServiceMock",
+        ["ngMockE2E"]);
 
-        var app = angular
-            .module("achievementServiceMock",
-            ["ngMockE2E", "config"]);
+    app.run(function($httpBackend) {
 
-        app.run(function ($httpBackend, config) {
-
-            var achievements = [
-                {
-                    ImmageUrl: "medal53.svg",
-                    Name: 'first achievement'
-                },
-                {
-                    ImmageUrl: "goal4.svg",
-                    Name: 'second achievement'
-                },
-                {
-                    ImmageUrl: "hand137.svg",
-                    Name: 'third achievement'
-                }
-            ];
-
-            var apiUrl = config.apiUrl;
-            $httpBackend.whenGET(apiUrl+'achievements/').respond(achievements);
-        });
+        var achievements = [
+            {
+                ImmageUrl: "medal53.svg",
+                Name: 'first achievement'
+            },
+            {
+                ImmageUrl: "goal4.svg",
+                Name: 'second achievement'
+            },
+            {
+                ImmageUrl: "hand137.svg",
+                Name: 'third achievement'
+            }
+        ];
+        var apiUrl = 'http://localhost:60704/api/v1/';
+        //var achievementsImagesUrl = 'BSL.Client/assets/images/football-icons/';
+        $httpBackend.whenGET(apiUrl + 'achivements').respond(achievements);
+        
+        // Pass through any requests for application files
+        $httpBackend.whenGET(/BSL.Client/).passThrough();
     });
-}());
+})();
--- Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service.js
@@ -3,19 +3,29 @@
 
     angular
      .module("common.services")
-     .factory("achievementService",
-             [achievementService]);
+     .factory("AchievementService",
+             achievementService);
 
     achievementService.$inject = ["config", '$http', '$q'];
 
     function achievementService(config, $http, $q)
     {
         return {
-            GetAllAchievements : getAllAchievements
+            GetAchievements : getAchievements
         }
 
-        function getAllAchievements() {
+        function getAchievements() {
+            var deferred = $q.defer();
 
+            $http.get(config.apiUrl + 'achivements')
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
         }
     }
 }());

fd603a318ed1b2e86e2cfb47058f6ff21f373040
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-17 00:35:18 -0500

    add getPlayerAchievements method to angular service for achievements

--- Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service.js
@@ -11,13 +11,28 @@
     function achievementService(config, $http, $q)
     {
         return {
-            GetAchievements : getAchievements
+            GetAchievements: getAchievements,
+            GetPlayerAchievements: getPlayerAchievements
         }
 
         function getAchievements() {
             var deferred = $q.defer();
 
-            $http.get(config.apiUrl + 'achivements')
+            $http.get(config.apiUrl + 'achievements')
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
+        }
+
+        function getPlayerAchievements(playerName) {
+            var deferred = $q.defer();
+
+            $http.get(config.apiUrl + 'achievements/player/'+ playerName)
                 .success(function (data, status, headers, config) {
                     deferred.resolve(data);
                 })

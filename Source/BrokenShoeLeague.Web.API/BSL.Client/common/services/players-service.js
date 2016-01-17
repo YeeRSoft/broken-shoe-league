baae74290c830afe8a11e46ae8ce32fde30af1b5
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-17 00:27:40 -0500

    create angular service and mock for players

---
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/players-service.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    "use strict";
+
+    angular
+     .module("common.services")
+     .factory("PlayersService",
+             playersService);
+
+    playersService.$inject = ["config", '$http', '$q'];
+
+    function playersService(config, $http, $q)
+    {
+        return {
+            GetPlayers: getPlayers
+        }
+
+        function getPlayers() {
+            var deferred = $q.defer();
+
+            $http.get(config.apiUrl + 'players')
+                .success(function (data, status, headers, config) {
+                    deferred.resolve(data);
+                })
+                .error(function (data, status, headers, config) {
+                    deferred.reject(status);
+                });
+
+            return deferred.promise;
+        }
+    }
+}());

d507f36805dc2d894972694a801c363f3d078428
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-16 22:43:01 -0500

    update AchievementsDetailsCtrl to call the angular service

--- Source/BrokenShoeLeague.Web.API/BSL.Client/app/achievements/controllers/achievementsDetailsCtrl.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/app/achievements/controllers/achievementsDetailsCtrl.js
@@ -2,10 +2,19 @@
     "use strict";
     angular.module("eeBrokenShoeLeague")
         .controller("AchievementsDetailsCtrl",
-        ['achievements', achievementsDetails]);
+        achievementsDetails);
 
-    function achievementsDetails(achievements) {
+    achievementsDetails.inject = ['AchievementService'];
+
+    function achievementsDetails(AchievementService) {
         var vm = this;
+
+        AchievementService.GetAchievements().then(
+            function(data) {
+                vm.AchievementList = data;
+            },
+            function(status) {
+            });
 
         vm.message = "hello";
     }

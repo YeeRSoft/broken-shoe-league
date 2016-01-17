e5efe7c9686810f319bfe4b2fc20c333a43168d5
Yassiel Enrique Oliva Carmona <yoliva0201@gmail.com>
2016-01-17 08:56:58 -0500

    fix regexp issue on achievements mock service

--- Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service-mock.js
+++ Source/BrokenShoeLeague.Web.API/BSL.Client/common/services/achievements-service-mock.js
@@ -9,85 +9,89 @@
 
         var achievements = [
             {
-                ImmageUrl: "medal53.svg",
-                Name: 'first achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "goal4.svg",
-                Name: 'second achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "goal4.svg",
+                Name: "second achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "hand137.svg",
-                Name: 'third achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "hand137.svg",
+                Name: "third achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "medal53.svg",
-                Name: 'first achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "goal4.svg",
-                Name: 'second achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "goal4.svg",
+                Name: "second achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "hand137.svg",
-                Name: 'third achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "hand137.svg",
+                Name: "third achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "medal53.svg",
-                Name: 'first achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
-            },
-                {
-                    ImmageUrl: "medal53.svg",
-                    Name: 'first achievement',
-                    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
-                },
-            {
-                ImmageUrl: "goal4.svg",
-                Name: 'second achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "hand137.svg",
-                Name: 'third achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "medal53.svg",
-                Name: 'first achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
-            },
-                {
-                    ImmageUrl: "medal53.svg",
-                    Name: 'first achievement',
-                    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
-                },
-            {
-                ImmageUrl: "goal4.svg",
-                Name: 'second achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "goal4.svg",
+                Name: "second achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "hand137.svg",
-                Name: 'third achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "hand137.svg",
+                Name: "third achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             },
             {
-                ImmageUrl: "medal53.svg",
-                Name: 'first achievement',
-                Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate..'
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
+            },
+            {
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
+            },
+            {
+                ImageUrl: "goal4.svg",
+                Name: "second achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
+            },
+            {
+                ImageUrl: "hand137.svg",
+                Name: "third achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
+            },
+            {
+                ImageUrl: "medal53.svg",
+                Name: "first achievement",
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis ante augue. Phasellus volutpat neque ac dui mattis vulputate.."
             }
         ];
 
-        var apiUrl = 'http://localhost:60704/api/v1/';
+        var apiUrl = "http://localhost:60704/api/v1/";
         //var achievementsImagesUrl = 'BSL.Client/assets/images/football-icons/';
-        $httpBackend.whenGET(apiUrl + 'achivements').respond(achievements);
+        $httpBackend.whenGET(apiUrl + "achievements").respond(achievements);
+
+        var playerAchievementsUrl = new RegExp(apiUrl + "achievements/player/" + "[a-zA-Z0-9]+", "");
+        $httpBackend.whenGET(playerAchievementsUrl).respond(achievements.slice(1, 4));
+
         
         // Pass through any requests for application files
         $httpBackend.whenGET(/BSL.Client/).passThrough();
